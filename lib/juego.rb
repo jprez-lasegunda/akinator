d9e95f97746cbd08d5ed437642bace5a696999fe
jprez-lasegunda <jprez@lasegunda.com.ar>
2019-11-14 13:03:26 -0300

    Refactor of global variables

---
+++ lib/juego.rb
@@ -0,0 +1,36 @@
+class Juego
+	def initialize
+			@cont = 1
+			@max = 3
+			@personas = lista_personas
+			@respuestas = [ ]
+			@preguntas = [
+				(Pregunta.new "es nene?", "hombre"), 
+				(Pregunta.new "es nena?", "mujer"),
+				(Pregunta.new "es chique?", "chique") 
+			]
+			rasgos = %w{chique castaño ojos_negros}
+			@elegido = Persona.new "Anitta", rasgos 
+	end
+	def cont
+		@cont
+	end
+	def max
+		@max
+	end
+	def personas
+		@personas
+	end
+	def respuestas
+		@respuestas
+	end
+	def elegido
+		@elegido
+	end
+	def preguntas
+		@preguntas 
+	end
+	def incrementar_cont
+		@cont += 1
+	end
+end
